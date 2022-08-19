031c16d624881e2eb6fdf477eca14596e1bc9ae9
Kelly Santos <98798428+Kellycns@users.noreply.github.com>
2022-08-19 14:34:39 -0300

    export form

--- react-front/src/components/molecules/index.jsx
+++ react-front/src/components/molecules/index.jsx
@@ -1,7 +1,9 @@
 import UserBio from "./UseBio";
 import UserPosts from "./UserPosts";
 import UserPostItem from "./UserPostItem";
+import Form from "./Form";
 
 export {UserBio};
 export {UserPosts};
 export {UserPostItem};
+export {Form};
