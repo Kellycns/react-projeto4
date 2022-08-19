73c68b721698de7764236fcabde5812a5ea10914
Kelly Santos <98798428+Kellycns@users.noreply.github.com>
2022-08-19 15:38:52 -0300

    post
    testando o formulário (post)

--- react-front/src/components/molecules/Form.jsx
+++ react-front/src/components/molecules/Form.jsx
@@ -1,38 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
+import {useForm} from 'react-hook-form'
+
 export default function Form(props) {
-    let navigate = useNavigate(); 
-    const submit = (e) =>{ 
-        let path = `/user/${props.id}`; 
-        navigate(path);
-    }
+
+    const {register, handleSubmit, formState: {erros}} = useForm()
+    const addpost = data => data console.log(data)
+    // let navigate = useNavigate(); 
+    // const submit = (e) =>{ 
+    //     let path = `/user/${props.id}`; 
+    //     navigate(path);
+    // }
   return (
     <div className='edit_container'>
         <img src={`https://cdn.traction.one/pokedex/pokemon/${props.id}.png`}/>
-        <form className='edit_form'>
-            <input
+        <form className='edit_form' onSubmit={handleSubmit(addpost)}>
+            <textarea
                 type="text"
                 name="name"
                 id="new-name"
                 className="campo"
                 placeholder="Novo nome"
             />
-            <input
-                type="text"
+            <textarea
+                type="username"
                 name="username"
                 id="new-username"
                 className="campo"
                 placeholder="Novo nome de usuario"
+                {...register("username")}
             />
-            <input
+            <textarea
                 type="text"
-                name="bio"
+                name="text"
                 id="new-bio"
                 className="campo"
                 placeholder="Nova bio"
+                {...register("text")}
             />
-            <button type="submit" value="salvar" className="botao_enviar" onClick={submit}>
+            <button type="submit" value="salvar" className="botao_enviar" >
             Salvar
             </button>
         </form>
